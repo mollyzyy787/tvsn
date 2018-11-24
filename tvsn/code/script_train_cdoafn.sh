acf7de45e01c5dadc29b2f576204014b9a9f3190
maxiaoba <maxiaoba@umich.edu>
2018-11-23 19:25:31 -0800

    test

--- tvsn/code/script_train_cdoafn.sh
+++ tvsn/code/script_train_cdoafn.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-OMP_NUM_THREADS=2 CUDA_VISIBLE_DEVICES=0 th train_cdoafn.lua --category car --modelString CDOAFN_SYM --imgscale 256 --batchSize 25 --lr 0.00005 --saveFreq 20 --maxEpoch 1 --gpu 0 --nThreads 8 --resume 0
-OMP_NUM_THREADS=2 CUDA_VISIBLE_DEVICES=0 th train_cdoafn.lua --category car --modelString CDOAFN_SYM --imgscale 256 --batchSize 25 --lr 0.00001 --saveFreq 20 --maxEpoch 2 --gpu 0 --nThreads 8 --resume 1
+OMP_NUM_THREADS=2 CUDA_VISIBLE_DEVICES=0 th train_cdoafn.lua --category car --modelString CDOAFN_SYM --imgscale 256 --batchSize 25 --lr 0.00005 --saveFreq 1 --maxEpoch 1 --gpu 0 --nThreads 8 --resume 0
+OMP_NUM_THREADS=2 CUDA_VISIBLE_DEVICES=0 th train_cdoafn.lua --category car --modelString CDOAFN_SYM --imgscale 256 --batchSize 25 --lr 0.00001 --saveFreq 1 --maxEpoch 2 --gpu 0 --nThreads 8 --resume 1
